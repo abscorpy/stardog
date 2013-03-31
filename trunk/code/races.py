19294ae2d4c64b80b9ebab141ccc22f19feb3b50
alexis.diomedi.12@sansano.usm.cl <alexis.diomedi.12@sansano.usm.cl@11a6aa44-0771-0476-ac7e-9e29f1f22079>
2013-03-31 02:48:49 +0000

    Made tweaks to landing system, in an attempt to make it less buggy... Added initial velocity to planet generated ships.

--- trunk/code/races.py
+++ trunk/code/races.py
@@ -15,13 +15,13 @@ class Race:
 	ships = []
 	shipScripts = []
 	script = None
-	
+
 	def __init__(self, game, name, color = (255,0,0)):
 		self.game = game
 		self.name = name
 		self.color = color
 		self.targetPlanet = None
-		
+
 	def update(self, dt):
 		#find geographic mean of planets in system:
 		x,y = 0,0
@@ -34,7 +34,7 @@ class Race:
 				x += planet.x
 				y += planet.y
 				count += 1
-			else: 
+			else:
 				enemies.append(planet)
 		if not enemies:
 			print "%s owns the entire system!"%(self,)
@@ -44,15 +44,15 @@ class Race:
 			y /= count
 		# set target to enemy closest to mean.
 		self.targetPlanet = min(enemies, key = lambda p: dist(p.x,p.y,x,y))
-		
-	
+
+
 	def updateShip(self, ship, dt):
 		if self.targetPlanet:
 			ship.planet = self.targetPlanet
-		
+
 	def updatePlanet(self, planet):
 		planet.population += (1 * planet.life * self.game.dt)
-		planet.buildProgress += (planet.population / 1000. * planet.resources 
+		planet.buildProgress += (planet.population / 1000. * planet.resources
 									* self.game.dt)
 		#print planet.name, planet.population, planet.buildProgress, planet.shipValue
 		#start building new ship:
@@ -60,7 +60,7 @@ class Race:
 			angle = randint(0, 360)
 			x = planet.x + cos(angle) * (planet.radius + 300)
 			y = planet.y + sin(angle) * (planet.radius + 300)
-			planet.shipInProgress = Strafebat(self.game, 
+			planet.shipInProgress = Strafebat(self.game,
 									x, y, color = self.color, race = self)
 			planet.shipValue = planet.shipInProgress.value
 		#complete ship construction:
@@ -68,8 +68,18 @@ class Race:
 			planet.buildProgress = 0
 			self.game.curSystem.add(planet.shipInProgress)
 			planet.shipInProgress.planet = planet
+			orbvel = sqrt(planet.g * self.game.curSystem.sun.mass * \
+			(2/planet.distance - 1/planet.SMa))
+			smi = planet.SMa * planet.p
+			vx = orbvel * -planet.SMa * math.sin(planet.EccAn) / sqrt((smi * \
+			math.cos(planet.EccAn)) ** 2 + (planet.SMa * math.sin(planet.EccAn)) ** 2)
+			vy = orbvel * smi * math.cos(planet.EccAn) / sqrt((smi * math.cos(planet.EccAn))
+			** 2 + (planet.SMa * math.sin(planet.EccAn)) ** 2)
+			planetvel = rotate(vx, vy, planet.LPe)
+			planet.shipInProgress.dx = planetvel[0]
+			planet.shipInProgress.dy = planetvel[1]
 			planet.shipInProgress = None
-			
-			
-			
-			
+
+
+
+
