338d60167d4eaa48d5b4d98414049c47a1d73700
TheMissingLint <themissinglint@gmail.com>
2013-05-19 23:25:18 +0000

    More tweaks to AI scripts. Strafebats strafe properly (around their target, but sometimes they target one ship and crash into another). Tinyfighters act like I wanted -- speed towards you shooting then reverse and retreat over and over.
    I think combat is getting fun again!
    Avoiding planets still needs some work.

--- code/tinyFighter.py
+++ code/tinyFighter.py
@@ -27,61 +27,37 @@ class TinyFighter(Ship):
 
 class TinyFighterScript(AIScript):
 	"""a script for tiny fighters."""
-	swarmRadius = 280
+	shootingRange = 400
 	retreatRadius = 300
-	shootingRange = 280
-	interceptSpeed = 200
-	def update(self, ship, dt):
-		busy = False
-		target = ship.target
-		if self.game.debug: print "tiny fighter in stage %s"%(ship.stage,)
-		
-		if ship.stage == "persue":	
-			#pursue until we match speed
-			
-			#only avoid planets in pursue stage.
-			planet = self.findClosestPlanet(Ship, ship.system.planets[0])
-			if planet and self.avoidPlanet(ship, planet):
-				return
-			
-			dx = target.dx - ship.dx
-			dy = target.dy - ship.dy
-			self.persue(ship, target)
-			if -50 < dx < 50 and -50 < dy < 50:
-				ship.stage = "charge"
-				
-		elif ship.stage == "charge":
-								
-			self.attack(ship, target)
-			
-			if ship.timeOut <= 0:
-				ship.stage = "persue"
-			ship.timeOut -= dt
-			if distance <= self.retreatRadius - self.swarmRadius:
-				ship.stage = "retreat"
-				
-		elif ship.stage == "retreat":
-			distance = dist(ship.x, ship.y, target.x, target.y)
-			if distance < self.retreatRadius:
-				if self.turnToTarget(ship, target, 180):
-					ship.forward()
-			if distance > self.swarmRadius:
-				ship.stage = "persue"
-				
-	def attack(self, ship, target):
-			distance = dist(ship.x, ship.y, target.x, target.y) - self.swarmRadius
-			turnTime = ship.moment / ship.torque * 180
-			
-			if self.relativeSpeed(ship, target) > - self.interceptSpeed:
-				if self.turnToTarget(ship, target):
-					ship.forward()
-			if distance <= self.shootingRange:
-					ship.shoot()
+	interceptSpeed = 300
 
-	def relativeSpeed(self, ship, target):
-		"""relativeSpeed2(ship, target) -> the relative speed between two
-		floaters. Note that this is negative if they are getting closer."""
-		#distance next second - distance this second (preserves sign):
-		return sqrt((ship.x + ship.dx - target.x - target.dx)**2 \
-								+ (ship.y + ship.dy - target.y - target.dy)**2) \
-						- sqrt((ship.x - target.x)**2 + (ship.y - target.y)**2)
+	def update(self, ship, dt):
+		"""tiny fighter retreats if it gets too close and 
+		has a max pursue speed."""
+		# find closest ship:
+		enemy = self.game.player
+		planet = self.findClosestPlanet(ship, ship.system.planets)
+		
+		if planet and self.avoidPlanet(ship, planet):
+			return
+		elif enemy and self.retreat(ship, enemy):
+			return
+		elif enemy and self.attack(ship, enemy):
+			return
+		elif (enemy and self.pursue(ship, enemy)):
+			return
+		elif self.idle(ship):
+			return
+			
+		assert("ship failed to idle?")
+		
+	def idle(self, ship):
+		pass	#no home planet to return to.
+		
+	def retreat(self, ship, enemy):
+		"""if within shooting range, turn at enemy and shoot it."""
+		if (dist2(ship, enemy) < self.retreatRadius ** 2 
+			or self.speedTowards(ship, enemy) > self.interceptSpeed):
+				self.flyAtDir(ship, relativeDir(ship, enemy) + 180)
+				return True
+		return False
